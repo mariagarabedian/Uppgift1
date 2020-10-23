1a779c888201a03c8f9d118daf265ebc3a6199b3
Maria Garabedian <maria.garabedian@learnet.se>
2020-10-23 12:11:51 +0200

    Add project files.

---
+++ Datalagring_Uppg1/Models/FileContentList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalagring_Uppg1.Models
+{
+    public class FileContentList : ObservableCollection<FileContent>
+    {
+        public FileContentList()
+        {
+
+        }
+        public FileContentList(string text)
+        {
+            Add(new FileContent(text));
+        }
+
+    }
+}
